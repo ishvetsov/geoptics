500470b7b8ef314e92df2ea7ea9398cdf4c93074
Shvetsov Ivan <onerv98@gmail.com>
2014-01-26 11:07:28 +0600

    Deposits in progress..

--- src/scripts/blocks/deposits_list/deposits_list.view.js
+++ src/scripts/blocks/deposits_list/deposits_list.view.js
@@ -10,11 +10,19 @@ define(function (require) {
         DepositsListEmptyTemplate = require('text!./deposits_list_empty.template.html');
 
     var DepositsListView = Marionette.ItemView.extend({
+        className: 'admin_deposits',
+
         getTemplate: function () {
             if (this.collection.length) {
                 return _.template(DepositsListTemplate);
             }
             return _.template(DepositsListEmptyTemplate);
+        },
+
+        onRender: function () {
+            this.binding = Rivets.bind(this.el, {
+                deposits: this.collection
+            });
         }
     });
 
