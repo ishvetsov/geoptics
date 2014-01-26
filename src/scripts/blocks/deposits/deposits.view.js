1f8eda0cc6fdab0a58d4c2aef01fabf9ab81e9d3
Shvetsov Ivan <onerv98@gmail.com>
2014-01-26 17:36:13 +0600

    Admin deposits, tree with open items dummy it's complete

--- src/scripts/blocks/deposits/deposits.view.js
+++ src/scripts/blocks/deposits/deposits.view.js
@@ -13,7 +13,7 @@ define(function (require) {
         className: 'admin_deposits',
 
         initialize: function () {
-            _.bindAll(this, 'clickDeposit');
+            _.bindAll(this, 'clickDeposit', 'clickCluster', 'clickWell');
         },
 
         getTemplate: function () {
@@ -24,29 +24,78 @@ define(function (require) {
         },
 
         onRender: function () {
-            this.binding = Rivets.bind(this.el, {
-                deposits: this.collection,
-                view: this
-            });
-
-            this._clusterTemplate = this.$el.find('.clusters-template');
+            this._bindData();
+            this._findTemplates();
         },
 
         clickDeposit: function (ev, data) {
-            var html = this._clusterTemplate
+            var html = this._depositTemplate
                 .clone()
                 .attr('hidden', false)
-                .html(),
+                .html();
 
-                $target = $(ev.currentTarget);
+            var $target = $(ev.currentTarget),
+                classes = $target.next().attr('class');
 
-            var classes = $target.next().attr('class');
-
-            if (classes && classes.search('clusters') > -1) {
+            if (classes && classes.search('deposit-info') > -1) {
                 $target.next().remove();
             } else {
                 $(html).insertAfter($target);
             }
+            
+            this._bindData();
+        },
+
+        clickCluster: function (ev, data) {
+            var html = this._clusterTemplate
+                .clone()
+                .attr('hidden', false)
+                .html();
+
+            var $target = $(ev.currentTarget),
+                classes = $target.next().attr('class');
+
+            if (classes && classes.search('cluster-info') > -1) {
+                $target.next().remove();
+            } else {
+                $(html).insertAfter($target);
+            }
+
+            this._bindData();
+        },
+
+        clickWell: function (ev, data) {
+            var html = this._wellTemplate
+                .clone()
+                .attr('hidden', false)
+                .html();
+
+            var $target = $(ev.currentTarget),
+                classes = $target.next().attr('class');
+
+            if (classes && classes.search('well-info') > -1) {
+                $target.next().remove();
+            } else {
+                $(html).insertAfter($target);
+            }
+
+            this._bindData();
+        },
+
+        _bindData: function () {
+            if (this.binding) {
+                this.binding.unbind();
+            }
+            this.binding = Rivets.bind(this.el, {
+                deposits: this.collection,
+                view: this
+            });
+        },
+
+        _findTemplates: function () {
+            this._depositTemplate = this.$el.find('.deposit-template');
+            this._clusterTemplate = this.$el.find('.cluster-template');
+            this._wellTemplate = this.$el.find('.well-template');
         }
     });
 
