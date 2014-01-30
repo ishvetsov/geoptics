9b53f3ea23a36ef91abd5bc2fbf2233f1080441d
Shvetsov Ivan <onerv98@gmail.com>
2014-01-30 19:40:50 +0600

    Remove highcharts

--- src/scripts/blocks/prime/graphics/prime_graphics.view.js
+++ src/scripts/blocks/prime/graphics/prime_graphics.view.js
@@ -6,24 +6,29 @@ define(function (require) {
     var Marionette = require('backbone.marionette'),
         Rivets = require('rivets'),
 
-        GraphicsTemplate = require('text!./prime_graphics.template.html');
+        Template = require('text!./prime_graphics.template.html');
 
     var GraphicsView = Marionette.ItemView.extend({
-        template: _.template(GraphicsTemplate),
-        className: 'graphics',
-        el: $('#container'),
+        template: _.template(Template),
 
-    render: (function () {
-            $('#container').highcharts({
-                chart: {
-                    type: 'line'
-                },
-                series: [{
-                    name: 'Tokyo',
-                    data: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6]
-                    }]
-            });
+        className: 'graphics',
+
+        ui: {
+            graphicsContainer: '#graphics-container'
+        },
+
+        onRender: (function () {
+            // $('#container').highcharts({
+            //     chart: {
+            //         type: 'line'
+            //     },
+            //     series: [{
+            //         name: 'Tokyo',
+            //         data: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6]
+            //         }]
+            // });
         })
     });
+
     return GraphicsView;
 });
